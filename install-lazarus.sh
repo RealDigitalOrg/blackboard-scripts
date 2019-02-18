7c58b01821d70e739f8ce81ed4022173a74abe02
root <root@localhost.localdomain>
2019-02-18 11:07:28 -0700

    Updated Lazarus installer script to create a couple of symbolic links.
    Lazarus will warn that crtbegin.o and crtend.o were not found.  The
    updated script creates symbolic links in the /usr/lib directory to
    these two files.

--- install-lazarus.sh
+++ install-lazarus.sh
@@ -5,3 +5,8 @@
 # Written by Rick Hoover on February 18th, 2019
 
 apt install lazarus
+
+# Create links to crtbegin.o and crtend.o
+cd /usr/lib
+ln -s gcc/arm-linux-gnueabihf/5/crtbegin.o .
+ln -s gcc/arm-linux-gnueabihf/5/crtend.o .
