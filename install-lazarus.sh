024803b20987f95d37629bf7075758fb9eca793a
Rick Hoover <rphoover@hotmail.com>
2020-08-15 13:28:00 -0600

    Updated script for newer versions of C.

--- install-lazarus.sh
+++ install-lazarus.sh
@@ -4,9 +4,24 @@
 # This script requires a network connection.
 # Written by Rick Hoover on February 18th, 2019
 
+CRT_DIR=gcc/arm-linux-gnueabihf
+C_VER=
+
+cd /usr/lib
+if [ -d ${CRT_DIR}/5 ]; then
+	C_VER=5
+	echo "5"
+fi
+if [ -d ${CRT_DIR}/7 ]; then
+	C_VER=7
+	echo "7"
+fi
+
 apt install lazarus
 
 # Create links to crtbegin.o and crtend.o
-cd /usr/lib
-ln -s gcc/arm-linux-gnueabihf/5/crtbegin.o .
-ln -s gcc/arm-linux-gnueabihf/5/crtend.o .
+if [ -n ${C_VER} ]; then
+	CRT_DIR=${CRT_DIR}/${C_VER}
+	ln -s ${CRT_DIR}/crtbegin.o .
+	ln -s ${CRT_DIR}/crtend.o .
+fi
